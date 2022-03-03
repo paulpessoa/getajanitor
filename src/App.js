b3f834a923bbc84fa072a4d813d326d6887bffff
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-03 01:17:50 -0300

    header style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <div className='main'>
+            <div className='center'>
+              <div className='menu'>
+                  <div className='logo'>
+                    <h3>Landingpage React</h3>
+                  </div>
+                  <div className=''>
+                      <a href='#'>Login</a>
+                  </div>
+              </div>
+            </div>
+        </div>
     </div>
   );
 }
