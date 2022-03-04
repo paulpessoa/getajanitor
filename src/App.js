92b7276206986bbeb17255c1a081483a8a9da79f
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-03 22:52:02 -0300

    form items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
         <div className='main'>
             <div className='center'>
+
               <div className='menu'>
                   <div className='logo'>
                     <h3>Landingpage React</h3>
@@ -12,6 +13,20 @@ function App() {
                   <div className='item-menu'>
                       <a href='#'>Login</a>
                   </div>
+              </div>
+
+              <div className='form'>
+                  <form>
+                      <h2>Contact us</h2>
+                      <div className='items-form'>
+                          <input type="text" placeholder='Name here...'/>
+                          <input type="email" placeholder='E-mail here...'/>
+                          <input type="tel" placeholder='Phone number...' required/>
+                          <input type="submit" value='Request Janitor'/>
+                     
+                      </div>
+                  </form>
+
               </div>
             </div>
         </div>
