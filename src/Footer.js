6508770eee31226ce6ec3332d1b7fc02ef8f2c2c
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-04 00:27:30 -0300

    component footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+function Footer () {
+    return (
+        <div className="center">
+        <footer className="footer">
+            <a href="https://www.linkedin.com/in/paulmspessoa"> Linkedin</a>
+            <a href="https://github.com/paulpessoa"> Github</a>
+            <p>Author: Paul Pessoa</p>
+        </footer>
+      </div>
+
+    );
+}
--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,31 @@
-function Main (){
-    return (
-    
-        <div className='main'>
-            <div className='center'>
-
-              <div className='menu'>
-                  <div className='logo'>
-                    <h3>Landingpage React</h3>
-                  </div>
-                  <div className='item-menu'>
-                      <a href='#'>Login</a>
-                  </div>
-              </div>
-
-              <div className='form'>
-                  <form>
-                      <h2>Contact us</h2>
-                      <div className='items-form'>
-                          <input type="text" placeholder='Name...'/>
-                          <input type="tel" placeholder='Phone...' required/>
-                          <input type="date" required/>
-                          <input type="submit" value='Request Janitor'/>
-                     
-                      </div>
-                  </form>
-
-              </div>
-            </div>
+function Main() {
+  return (
+    <div className="main">
+      <div className="center">
+        <div className="menu">
+          <div className="logo">
+            <h3>Landingpage React</h3>
+          </div>
+          <div className="item-menu">
+            <a href="#">Login</a>
+          </div>
         </div>
-    );
+
+        <div className="form">
+          <form>
+            <h2>Contact us</h2>
+            <div className="items-form">
+              <input type="text" placeholder="Name..." />
+              <input type="tel" placeholder="Phone..." required />
+              <input type="date" required />
+              <input type="submit" value="Request Janitor" />
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Main;
