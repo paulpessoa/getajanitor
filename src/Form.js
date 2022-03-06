b5c70fe5aa9edda717a294d194ca6206d311a78d
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-06 05:10:29 -0300

    copy email, remove captcha, subject email

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,27 @@
 function Form() {
-    return (
-      <div className="contact">
-        
-            <div className="form">
-                <form action="https://formsubmit.co/paulmspessoa@gmail.com" method="POST">
-                <h2>schedule cleaning</h2>
-                <div className="items-form">
-                    <input type="text" name="name" placeholder="Name..." />
-                    <input type="tel" name="phone" placeholder="Phone..." required />
-                    <input type="date" name="date" required />
-                    <input type="mail" name="mail" placeholder="your@email.com"/>
-                    <input type="submit" value="Request Janitor" />
-                </div>
-                </form>
-            </div>
-        </div>
-      
-    );
-  }
-  
-  export default Form;
-  
+  return (
+    <div className="contact">
+      <div className="form">
+        <form
+          action="https://formsubmit.co/paulmspessoa@gmail.com"
+          method="POST"
+        >
+          <h2>schedule cleaning</h2>
+          <div className="items-form">
+            <input type="hidden" name="_subject" value="Schedule a Janitor"/>
+            <input type="hidden" name="_captcha" value="false"/>
+            <input type="hidden" name="_autoresponse" value="your custom message"/>
+
+            <input type="text" name="name" placeholder="Name..." required/>
+            <input type="tel" name="phone" placeholder="Phone..." required />
+            <input type="date" name="date" required />
+            <input type="mail" name="mail" placeholder="your@email.com" required/>
+            <input type="submit" value="Request Janitor" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
