78ab31ce0cf36eb70f435cb5ff546b9df3761e4a
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-06 04:57:26 -0300

    send email

--- src/Form.js
+++ src/Form.js
@@ -3,12 +3,13 @@ function Form() {
       <div className="contact">
         
             <div className="form">
-                <form>
+                <form action="https://formsubmit.co/paulmspessoa@gmail.com" method="POST">
                 <h2>schedule cleaning</h2>
                 <div className="items-form">
-                    <input type="text" placeholder="Name..." />
-                    <input type="tel" placeholder="Phone..." required />
-                    <input type="date" required />
+                    <input type="text" name="name" placeholder="Name..." />
+                    <input type="tel" name="phone" placeholder="Phone..." required />
+                    <input type="date" name="date" required />
+                    <input type="mail" name="mail" placeholder="your@email.com"/>
                     <input type="submit" value="Request Janitor" />
                 </div>
                 </form>
